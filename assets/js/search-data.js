9b3d0af7808bbd12503d5f0913e08e4bd61fb5db
xwmaxwma <96612872+xwmaxwma@users.noreply.github.com>
2025-05-08 06:50:09 +0000

    Deploying to gh-pages from @ xwmaxwma/xwmaxwma.github.io@7482e439996d5ee5c2b7c9c7a8c66b803e9cebd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Waiting for update.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
